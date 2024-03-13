6ad11156ea2cb4109de011a8b05f7a45308d6e5d
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-13 20:55:23 +0200

    getting there

---
+++ COS3711/assignment1_question3/example.cpp
@@ -0,0 +1,4 @@
+#include <QtGui>
+#include <QtCore>
+
+
--- COS3711/assignment1_question3/main.cpp
+++ COS3711/assignment1_question3/main.cpp
@@ -7,29 +7,57 @@
 #include <QTreeView>
 #include <QModelIndex>
 #include <QDebug>
+#include <QTableView>
+#include <QStandardItemModel>
+
+void createModel()
+{
+    QStandardItemModel *model = new QStandardItemModel(2,2);
+    QStringList list;
+    list << "Col 1" << "Col 2";
+    model->setHorizontalHeaderLabels(list);
+
+    for (int row=0;row<model->rowCount(); ++row)
+    {
+        for (int column = 0;column<model->columnCount();++column)
+        {
+            QStandardItem *item = new QStandardItem(QString("row %0, column %1").arg(row).arg(column));
+            model->setItem(row, column, item);
+        }
+    }
+
+    QTableView *view = new QTableView;
+    view->setModel(model);
+    view->show();
+}
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
-    QSplitter *splitter = new QSplitter;
+//    QSplitter *splitter = new QSplitter;
 
-    QFileSystemModel *model = new QFileSystemModel;
-    model->setRootPath(QDir::currentPath());
-
-
-    QTreeView *tree = new QTreeView(splitter);
-    tree->setModel(model);
-    tree->setRootIndex(model->index(QDir::currentPath()));
-
-    QListView *list = new QListView(splitter);
-    list->setModel(model);
-    list->setRootIndex(model->index(QDir::currentPath()));
+//    QFileSystemModel *model = new QFileSystemModel;
+//    model->setRootPath(QDir::currentPath());
 
 
 
+//    QTreeView *tree = new QTreeView(splitter);
+//    tree->setModel(model);
+//    tree->setRootIndex(model->index(QDir::currentPath()));
 
-    splitter->setWindowTitle("Two views onto the same file system model");
-    splitter->show();
+//    QListView *list = new QListView(splitter);
+//    list->setModel(model);
+//    list->setRootIndex(model->index(QDir::currentPath()));
+
+
+
+
+//    splitter->setWindowTitle("Two views onto the same file system model");
+//    splitter->show();
+
+    createModel();
+
     return a.exec();
 
 }
