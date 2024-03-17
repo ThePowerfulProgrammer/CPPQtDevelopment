1debc3a6c02cb5d3d5581ecabb508775029311e5
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-17 19:55:39 +0200

    Just DS & Algo

--- MVD/SimpleTableMVC/mydialog.cpp
+++ MVD/SimpleTableMVC/mydialog.cpp
@@ -1,38 +1,106 @@
 #include "mydialog.h"
 #include <QDebug>
 #include <QStandardItem>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QStringList>
+#include <QList>
 
 myDialog::myDialog(QWidget *parent): QDialog(parent)
 {
-    model = new QStandardItemModel(4,4);
-    for (int row = 0; row < model->rowCount(); ++row)
-    {
-        for (int column = 0; column < model->columnCount(); ++column)
-        {
-            QStandardItem *item = new QStandardItem(QString("row %0, column %1").arg(row).arg(column));
-            model->setItem(row, column, item);
-        }
-    }
+    // create the first horizontal box
+    composerLabel = new QLabel("Composer",this);
+    albumTitle = new QLabel("Album title",this);
+    replacementCost = new QLabel("Replacement cost(R)",this);
+    rating = new QLabel("Rating",this);
+
+
+    QHBoxLayout *firstRow = new QHBoxLayout;
+    firstRow->addWidget(composerLabel);
+    firstRow->addSpacing(30);
+    firstRow->addWidget(albumTitle);
+    firstRow->addWidget(replacementCost);
+    firstRow->addWidget(rating);
+
+    // create the 2nd row
+    addComposer = new QLineEdit(this);
+    addComposer->setPlaceholderText("Composer");
+
+    addAlbum = new QLineEdit(this);
+    addAlbum->setPlaceholderText("Album");
+
+    addReplacement = new QDoubleSpinBox(this);
+    addReplacement->setMinimum(0.00);
+    addReplacement->setMaximum(999.99);
+
+    addRating = new QSpinBox(this);
+    addRating->setMinimum(0);
+    addRating->setMaximum(10);
+
+    add = new QPushButton("Add",this);
+
+    QHBoxLayout *secondRow = new QHBoxLayout;
+    secondRow->addWidget(addComposer);
+    secondRow->addWidget(addAlbum);
+    secondRow->addWidget(addReplacement);
+    secondRow->addWidget(addRating);
+    secondRow->addWidget(add);
+
+
+    // 3rd row: Create my model add the data and set the tableview
+    model = new QStandardItemModel(this);
+    QStringList labels;
+    labels << "Composer" << "Album title" << "Replacement cost(R)" << "Rating";
+    model->setHorizontalHeaderLabels(labels);
+
 
     tableView = new QTableView(this);
-
     tableView->setModel(model);
+    tableView->setColumnWidth(2,tableView->columnWidth(2)+50);
+    QHBoxLayout *thirdRow = new QHBoxLayout;
+    thirdRow->addWidget(tableView);
 
-//    I want to create a singal and slot connection here that when I click a cell in my table, it streams the item to the console
-    connect(tableView,SIGNAL(doubleClicked(QModelIndex)), this,SLOT(output());
+    connect(add,SIGNAL(clicked()),this,SLOT(addData()));
 
-    setWindowTitle("MVC");
-    resize(400,400);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(firstRow);
+    mainLayout->addLayout(secondRow);
+    mainLayout->addLayout(thirdRow);
+
+
+    setLayout(mainLayout);
 }
 
 
-void myDialog::output()
+void myDialog::addData()
 {
-    QStandardItem *item = tableView->indexAt(model->indexFromItem())
+    // I want to add a new row to my model, I will use the indiviual widgets I have access to
+    // Get the values from the widgets
+    QString composer_text = addComposer->text();
+    QString album_text = addAlbum->text();
+    double replacement_value = addReplacement->value();
+    int rating_value = addRating->value();
+
+    addComposer->clear();
+    addComposer->setPlaceholderText("Composer");
+    addAlbum->clear();
+    addAlbum->setPlaceholderText("Album");
+    addReplacement->setValue(0.00);
+    addRating->setValue(0);
+
+    // Create QStandardItem objects
+    QStandardItem* composer_item = new QStandardItem(composer_text);
+    QStandardItem* album_item = new QStandardItem(album_text);
+    QStandardItem* replacement_item = new QStandardItem(QString::number(replacement_value));
+    QStandardItem* rating_item = new QStandardItem(QString::number(rating_value));
+
+    // Insert a new row into the model
+    int row = model->rowCount();
+    model->insertRow(row, QList<QStandardItem*>() << composer_item << album_item << replacement_item << rating_item);
 }
 
 myDialog::~myDialog()
 {
-    delete model;
-    delete tableView;
+
 }
--- MVD/SimpleTableMVC/mydialog.h
+++ MVD/SimpleTableMVC/mydialog.h
@@ -3,7 +3,13 @@
 #include <QDialog>
 #include <QTableView>
 #include <QStandardItemModel>
-
+#include <QString>
+#include <QSpinBox>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QDoubleSpinBox>
+#include <QSpinBox>
 
 class myDialog : public QDialog
 {
@@ -14,10 +20,28 @@ public:
 
 
 public slots:
-    void output();
+    void addData();
 private:
+    // 1st row: 4 labels
+    QLabel *composerLabel;
+    QLabel *albumTitle;
+    QLabel *replacementCost;
+    QLabel *rating;
+
+    // 2nd row: 4 input widgets but these will have to be delegates and 1 button
+    QLineEdit *addComposer;
+    QLineEdit *addAlbum;
+    QDoubleSpinBox *addReplacement;
+    QSpinBox *addRating;
+    QPushButton *add;
+
+    // 3rd row
     QStandardItemModel *model;
     QTableView *tableView;
+
+    // 4th row
+    QPushButton *deleteButton;
+
 };
 
 #endif // MYDIALOG_H
