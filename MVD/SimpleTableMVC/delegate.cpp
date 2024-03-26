bd99dbcfbaa1685e6fa54568f918b5f79b2c58e9
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-03-26 16:23:53 +0200

    Learning

--- MVD/SimpleTableMVC/delegate.cpp
+++ MVD/SimpleTableMVC/delegate.cpp
@@ -1,11 +1,45 @@
 #include "delegate.h"
 #include <QSlider>
 #include <QApplication>
+#include <QColor>
+#include <QPainter>
+#include <QColor>
+#include <QFile>
+
+
 
 Delegate::Delegate(QObject *parent) :
     QStyledItemDelegate(parent)
 {
 
+}
+
+
+void Delegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    // last column is rating :^)
+    if (index.column() == 3)
+    {
+
+        QStyleOptionProgressBar progressBar;
+        progressBar.rect = option.rect;
+        progressBar.minimum = 0;
+        progressBar.maximum = 10;
+        progressBar.progress = index.data(Qt::EditRole).toInt();
+
+
+
+        QProgressBar bar;
+        bar.setRange(progressBar.minimum, progressBar.maximum);
+        bar.setValue(progressBar.progress);
+        bar.setTextVisible(false);
+        QFile file("C:/Users/ashis/OneDrive/Desktop/Qt/MVD/SimpleTableMVC/style.qss");
+        file.open(QFile::ReadOnly);
+        QString styleSheet = QLatin1String(file.readAll());
+        bar.setStyleSheet(styleSheet);
+
+        QApplication::style()->drawControl(QStyle::CE_ProgressBar, &progressBar, painter, &bar);
+    }
 }
 
 
@@ -17,6 +51,12 @@ QWidget* Delegate::createEditor(QWidget *parent, const QStyleOptionViewItem &opt
     barEditor->setMinimum(0);
     barEditor->setMaximum(10);
     barEditor->setOrientation(Qt::Horizontal);
+
+    QFile file("C:/Users/ashis/OneDrive/Desktop/Qt/MVD/SimpleTableMVC/style.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+
+    barEditor->setStyleSheet(styleSheet);
 
     return barEditor;
 
@@ -30,15 +70,17 @@ void Delegate::setEditorData(QWidget *editor, const QModelIndex &index) const
     int value = index.model()->data(index,Qt::EditRole).toInt();
 
     // how do I want to edit data?
-    // I need to connect signal and slots to rating and replacement cost
+    // I need to connect signal and slots
 
     QProgressBar *slider = static_cast<QProgressBar*>(editor);
     slider->setOrientation(Qt::Horizontal);
     slider->setValue(value);
-    slider->setStyleSheet("QProgressBar::chunk {"
-                          "     background-color: red;"
-                          "}");
 
+    QFile file("C:/Users/ashis/OneDrive/Desktop/Qt/MVD/SimpleTableMVC/style.qss");
+    file.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(file.readAll());
+
+    slider->setStyleSheet(styleSheet);
 }
 
 
@@ -59,23 +101,3 @@ void Delegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem 
 }
 
 
-void Delegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const
-{
-    // last column is rating :^)
-    if (index.column() == 3)
-    {
-        QStyleOptionProgressBar progressBar;
-        progressBar.rect = option.rect;
-        progressBar.minimum = 0;
-        progressBar.maximum = 10;
-        progressBar.progress = index.data(Qt::EditRole).toInt();
-
-
-        QProgressBar bar;
-        bar.setRange(progressBar.minimum, progressBar.maximum);
-        bar.setValue(progressBar.progress);
-        bar.setTextVisible(false);
-
-        QApplication::style()->drawControl(QStyle::CE_ProgressBar, &progressBar, painter, &bar);
-    }
-}
