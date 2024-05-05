74ee5022f6d1f211a1bd69b16cdaebafb492f886
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-05-05 21:45:28 +0200

    broken state

--- databaseInteraction/main.cpp
+++ databaseInteraction/main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QtSql/QSqlDatabase>
+
 #include <QDebug>
 
 
@@ -7,18 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setDatabaseName("test");
-    db.setHostName("localhost");
-    db.setDatabaseName("customdb");
-    db.setUserName("mojito");
-    db.setPassword("J0a1m8");
-    bool ok = db.open();
-
-    if (ok)
-        qDebug() << "Connected to MySQL!";
-    else
-        qDebug() << "Failed to connect to MySQL.";
-
+    qDebug() << "There should be drivers here" << "\n";
     return a.exec();
 }
