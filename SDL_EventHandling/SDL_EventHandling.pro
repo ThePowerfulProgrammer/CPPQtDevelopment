68ee93332751e45cfa6c5720adc35a3fb285740b
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-01 21:22:25 +0200

    another one in the books

---
+++ SDL_EventHandling/SDL_EventHandling.pro
@@ -0,0 +1,28 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2024-06-01T20:52:01
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = SDL_EventHandling
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+
+LIBS += -L"C:/Users/ashis/OneDrive/Desktop/SDL Development/SDL2-devel-2.30.3-mingw/SDL2-2.30.3/i686-w64-mingw32/lib" -lSDL2
+
+INCLUDEPATH += "C:/Users/ashis/OneDrive/Desktop/SDL Development/SDL2-devel-2.30.3-mingw/SDL2-2.30.3/i686-w64-mingw32/include/SDL2"
+
+OTHER_FILES += \
+    ../../../../Downloads/03_event_driven_programming/03_event_driven_programming/x.bmp
+
+
---
+++ SDL_EventHandling/main.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+#include <SDL.h>
+#include <stdio.h>
+
+#undef main
+
+using namespace std;
+
+string imgPath = "C:/Users/ashis/Downloads/03_event_driven_programming/03_event_driven_programming/x.bmp";
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+
+// Start up SDL
+
+bool init();
+
+// loads Media
+bool loadMedia();
+
+// Free media and close SDL
+void close();
+
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gXOut = NULL;
+
+
+bool init()
+{
+    bool success = true;
+
+    if (SDL_Init( SDL_INIT_VIDEO) < 0 )
+    {
+        printf("SDL Could not init %s \n", SDL_GetError());
+        success =  false;
+    }
+    else
+    {
+        gWindow = SDL_CreateWindow("SDL Events Handling", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if (gWindow == NULL)
+        {
+            printf("Window could not be created %s \n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia()
+{
+    bool success = true;
+
+    gXOut = SDL_LoadBMP("C:/Users/ashis/Downloads/03_event_driven_programming/03_event_driven_programming/x.bmp");
+    if (gXOut == NULL)
+    {
+        printf("SDL CANNOT LOAD IMAGE \n");
+        success = false;
+
+    }
+    return success;
+}
+
+void close()
+{
+    SDL_FreeSurface(gXOut);
+    gXOut = NULL;
+
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    SDL_Quit();
+}
+
+int main(int argc, char *argv[])
+{
+    if (!init())
+    {
+        printf("Failed to int \n");
+    }
+    else
+    {
+        if (!loadMedia())
+        {
+            printf("Failed to load Media \n");
+        }
+        else
+        {
+            bool quit = false;
+
+            SDL_Event e;
+
+            while (!quit)
+            {
+                while (SDL_PollEvent(&e) != 0)
+                {
+                    if (e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+                SDL_BlitSurface(gXOut,NULL, gScreenSurface, NULL);
+
+                SDL_UpdateWindowSurface( gWindow );
+            }
+        }
+    }
+
+    close();
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
