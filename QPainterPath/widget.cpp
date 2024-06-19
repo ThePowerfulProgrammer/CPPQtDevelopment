584eeff13c7eba37f067eb32283a9e0378fe15d9
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-19 14:58:30 +0200

    QPainterPath Working understanding

--- QPainterPath/widget.cpp
+++ QPainterPath/widget.cpp
@@ -21,6 +21,7 @@ Widget::~Widget()
 void Widget::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
+    painter.setBrush(Qt::cyan);
     QPainterPath path;
 
     path.addRect(100,100,100,100);
@@ -34,42 +35,33 @@ void Widget::paintEvent(QPaintEvent *event)
 
     path.lineTo(150,150);
 
-    qDebug() << path.currentPosition() << "\n";
 
     path.lineTo(150,150);
 
-    qDebug() << path.currentPosition() << "\n";
 
     path.moveTo(150,250);
     path.lineTo(500,250);
 
     path.addEllipse(QPoint(150,300),50,50);
 
-    qDebug() << "Prior to distance: " << path.currentPosition() << "\n";
-    qDebug() << path.currentPosition() << "\n";
 
     path.moveTo(150,350);
     path.lineTo(500,350);
 
-    path.addEllipse(QPoint(500,300), 50,50);
-
-
     path.moveTo(300,450);
     path.addEllipse(QPoint(300,450),50,50);
 
-    qDebug() << "MOST CUR: " << path.currentPosition() << "\n";
 
     path.moveTo(300,400);
     path.lineTo(650,400);
+    path.addEllipse(QPoint(650,450),50,50);
+
     path.moveTo(300,500);
     path.lineTo(650,500);
 
-    path.addEllipse(QPoint(650,450),50,50);
-    qDebug() << "MOST CUR: " << path.currentPosition() << "\n";
-
     path.addEllipse(QPoint(500,300), 50,50);
 
-    painter.setBrush(Qt::cyan);
+
     painter.drawPath(path);
 }
 
