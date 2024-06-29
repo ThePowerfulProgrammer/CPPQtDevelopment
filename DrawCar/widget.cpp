2b74a055c7e6a287081437b849583369756f9f07
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-06-29 22:34:39 +0200

    Drawing manually is harsh

--- DrawCar/widget.cpp
+++ DrawCar/widget.cpp
@@ -1,5 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QPainter>
+#include <QDebug>
+
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
@@ -16,4 +19,60 @@ Widget::~Widget()
 
 void Widget::paintEvent(QPaintEvent *event)
 {
+    // create vars
+    QPainterPath path;
+    QPainter painter(this);
+    painter.setPen(Qt::blue);
+
+    // create tyres
+    qDebug() << path.currentPosition() << "\n";
+    path.moveTo(QPoint(150,250));
+    path.addEllipse(path.currentPosition(),30,30);
+    path.moveTo(150,250);
+    qDebug() << path.currentPosition() << "\n";
+
+    path.addEllipse(path.currentPosition(),50,50);
+    qDebug() << path.currentPosition() << "\n";
+    painter.drawPath(path);
+
+    path.translate(400,0);
+    painter.drawPath(path);
+
+    // create front bonnet
+    qDebug() << "Before front: "<< path.currentPosition() << "\n";
+    path.moveTo(620,250);
+    path.lineTo(700,250);
+    path.lineTo(675,150);
+    path.lineTo(525,150);
+    painter.drawPath(path);
+    qDebug() << path.currentPosition() << "\n";
+
+    // create back boot
+    path.moveTo(150,150);
+    path.lineTo(80,150);
+    path.lineTo(50,250);
+    path.lineTo(80,250);
+    painter.drawPath(path);
+    qDebug() << path.currentPosition() << "\n";
+
+    // complete the lower body
+    QRectF rectangle(80,175,150,150);
+    int startAngle = 0*16;
+    int spanAngle = 180 * 16;
+    painter.drawArc(rectangle, startAngle, spanAngle);
+
+    path.moveTo(229,250);
+    path.lineTo(470,250);
+    painter.drawPath(path);
+
+    qDebug() << rectangle.x() << "\n";
+    qDebug() << rectangle.y() << "\n";
+    rectangle.translate(390,0);
+    painter.drawArc(rectangle, startAngle, spanAngle);
+
+    path.moveTo(525,150);
+    path.lineTo(440,80);
+    path.lineTo(200,80);
+    path.lineTo(150,150);
+    painter.drawPath(path);
 }
