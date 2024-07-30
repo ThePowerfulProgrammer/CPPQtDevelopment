48c93f5cc97b695a0779003fb858e3e6f5af1adc
ThePowerfulProgrammer <ashishr0301@gmail.com>
2024-07-30 18:41:07 +0200

    Now Editable, Qt::Roles are functions based

--- customModelReadOnly/personmodel.cpp
+++ customModelReadOnly/personmodel.cpp
@@ -34,6 +34,11 @@ QVariant PersonModel::data(const QModelIndex &index, int role) const
         return person->names() + " " + QString::number(person->age()) + " " + person->favoriteColor();
     }
 
+    if (role == Qt::EditRole)
+    {
+        return person->names() + " " + QString::number(person->age()) + " " + person->favoriteColor();
+    }
+
     if (role == Qt::ToolTipRole)
     {
         return person->names() + " " + QString::number(index.row());
@@ -48,3 +53,67 @@ PersonModel::~PersonModel()
 {
     qDeleteAll(persons);
 }
+
+
+bool PersonModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if (!index.isValid())
+    {
+        return false;
+    }
+
+    Person *person = persons[index.row()];
+    bool somethingChanged = false;
+
+    switch (role)
+    {
+        case Qt::EditRole:
+        {
+            if (person->names() != value.toString())
+            {
+                person->setNames(value.toString());
+                somethingChanged = true;
+
+            }
+        }
+    }
+
+
+    if (somethingChanged)
+    {
+        emit dataChanged(index, index);
+        return true;
+    }
+
+    return false;
+
+
+}
+
+QVariant PersonModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole)
+    {
+        return QVariant();
+    }
+
+    if (orientation == Qt::Horizontal)
+    {
+        return QString("Person names");
+    }
+
+    return QString("Person %1").arg(section);
+}
+
+Qt::ItemFlags PersonModel::flags(const QModelIndex &index) const
+{
+    if (!index.isValid())
+    {
+        return QAbstractItemModel::flags(index);
+    }
+    return QAbstractItemModel::flags(index) | Qt::ItemIsEditable;
+}
+
+
+
+
--- customModelReadOnly/personmodel.h
+++ customModelReadOnly/personmodel.h
@@ -22,6 +22,10 @@ private:
 public:
     int rowCount(const QModelIndex &parent) const;
     QVariant data(const QModelIndex &index, int role) const;
+
+    bool setData(const QModelIndex &index, const QVariant &value, int role);
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
 };
 
 #endif // PERSONMODEL_H
